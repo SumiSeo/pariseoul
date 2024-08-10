4ff60a16cebe7e0bf2fe11ac13b9b9669c2d548a
Sumi <qkobr94@gmail.com>
2024-08-10 20:15:05 +0900

    feat/migrate-models

--- backend/myapp/models.py
+++ backend/myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Tour(models.Model):
+    origin_country = models.CharField(max_length=64)
+    destination_country = models.CharField(max_length=64)
+    number_of_nights = models.IntegerField()
+    price=models.IntegerField()
+    
+    #this is a string representation of the tours
+    def __str__(self):
+        return (f"ID:{self.id} : FROM {self.origin_country} TO {self.destination_country}, {self.number_of_nights} for {self.price}")
+    
